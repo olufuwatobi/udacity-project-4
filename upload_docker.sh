263d73ce8146f90af13c702ba924c49f4cc3a631
oluwatobi Olufuwa <olufuwatobi@gmail.com>
2020-04-17 20:36:15 +0100

    updated the upload.sh file

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=olutobi/udacity-project-four:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u olutobi 
+docker tag udacity-project-four $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
